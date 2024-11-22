75b02348df0d7207348e6147f3b62cabf5898aa0
Djlanav <88490984+Djlanav@users.noreply.github.com>
2024-11-22 13:05:00 -0600

    Delete rust-src/target directory

--- rust-src/target/debug/build/proc-macro2-af373eff3cd1001c/build_script_build-af373eff3cd1001c.d
+++
@@ -1,5 +0,0 @@
-/home/cmh/Documents/Godot Projects/fps-project/rust-src/target/debug/build/proc-macro2-af373eff3cd1001c/build_script_build-af373eff3cd1001c: /home/cmh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs
-
-/home/cmh/Documents/Godot Projects/fps-project/rust-src/target/debug/build/proc-macro2-af373eff3cd1001c/build_script_build-af373eff3cd1001c.d: /home/cmh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs
-
-/home/cmh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs:
--- rust-src/target/debug/deps/rust_src.d
+++
@@ -1,7 +0,0 @@
-/home/cmh/Documents/Godot Projects/fps-project/rust-src/target/debug/deps/librust_src.so: src/lib.rs src/audio_handling.rs src/utils.rs
-
-/home/cmh/Documents/Godot Projects/fps-project/rust-src/target/debug/deps/rust_src.d: src/lib.rs src/audio_handling.rs src/utils.rs
-
-src/lib.rs:
-src/audio_handling.rs:
-src/utils.rs:
